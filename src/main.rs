f7f4685f16d42cf0fd807ae74d2f23cd86fe5c1f
Zach Mitchell <zmitchell@fastmail.com>
2020-04-07 18:23:05 -0400

    feat(routes): Define basic route filters

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let roll = warp::path!("roll" / String)
-        .map(|dice| format!("Roll {}", dice));
-
-    warp::serve(roll)
+    let roll = warp::path("roll");
+    let dice_str = warp::path::param().and(warp::path::end());
+    let routes = roll.and(dice_str).map(|dice: String| {
+        format!("dice: {}", dice)
+    });
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
